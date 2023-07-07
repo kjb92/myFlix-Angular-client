727ff26a071c0a0344debde94191bdcc194e42eb
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-07 16:22:40 +0200

    refactor: Change styling of profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
@@ -17,13 +18,14 @@ export class UserProfileComponent implements OnInit {
     username: '',
     password: '',
     email: '',
-    birthday: '',
+    birthday: null as string | null,
   };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public router: Router,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit(): void {
@@ -38,42 +40,44 @@ export class UserProfileComponent implements OnInit {
       // console.log(resp);
       this.updatedUser.username = this.user.username;
       this.updatedUser.email = this.user.email;
-      this.updatedUser.birthday = this.user.birthday;
+      this.updatedUser.birthday = this.datePipe.transform(
+        this.user.birthday,
+        'yyyy-MM-dd'
+      );
       return this.user;
     });
   }
 
   //Function to delete user
   deleteUser(): void {
-    if (
-      confirm(
-        'Are you sure you want to delete your account?'
-      )
-    ) {
-      this.router.navigate(['welcome']).then(() => {
-        this.snackBar.open(
-          'Your account was successfully deleted!',
-          'OK',
-          {
-            duration: 4000,
-          }
-        );
-      });
+    if (confirm('Are you sure you want to delete your account?')) {
       this.fetchApiData.deleteUser().subscribe((result) => {
         localStorage.clear();
+      });
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('Your account was successfully deleted!', 'OK', {
+          duration: 4000,
+        });
       });
     }
   }
 
   //Function to update user data
   updateUserData(): void {
-    this.fetchApiData.editUser(this.updatedUser).subscribe((result) => {
-      console.log(result);
-      this.snackBar.open('User profile was successfuly updated', 'OK', {
+    // Check if the password field is empty
+    if (!this.updatedUser.password) {
+      this.snackBar.open('Please enter a password', 'OK', {
         duration: 4000,
       });
-      localStorage.setItem('user', result);
-      window.location.reload();
+      return; // Stop execution if password is empty
+    }
+
+    this.fetchApiData.editUser(this.updatedUser).subscribe((result) => {
+      console.log(result);
+      this.snackBar.open('User profile was successfully updated', 'OK', {
+        duration: 4000,
+      });
+      localStorage.setItem('user', JSON.stringify(result));
     });
   }
 }
